6e9efeb658a7246be285b24035e25ed648b76e55
Axel Wienberg <axel@wuenschenswert.net>
2020-06-19 14:19:57 +0200

    do not upload error response files

--- upload-files.sh
+++ upload-files.sh
@@ -10,7 +10,11 @@ while [ $N -ge 0 ] ; do
   DIR=$(date +%Y "--date=$D")/$(date +%m "--date=$D")
   FILE=$(date +%Y%m%d "--date=$D").TXT
   # wget -x -nH --cut-dirs=1 "--directory-prefix=${SOLAR_HEAT_DIR}" http://${WLAN_SD_IP}/SC514/${DIR}/${FILE}
-  ssh solarinbox@solar.ferne-gefil.de "mkdir -p SC514/${DIR}"
-  scp ${SOLAR_HEAT_DIR}/${DIR}/${FILE} solarinbox@solar.ferne-gefil.de:SC514/${DIR}/${FILE}
+  if head -1 ${SOLAR_HEAT_DIR}/${DIR}/$FILE | grep -q DOCTYPE ; then
+    echo Error response file, not uploading: ${SOLAR_HEAT_DIR}/${DIR}/$FILE
+  else
+    ssh solarinbox@solar.ferne-gefil.de "mkdir -p SC514/${DIR}"
+    scp ${SOLAR_HEAT_DIR}/${DIR}/${FILE} solarinbox@solar.ferne-gefil.de:SC514/${DIR}/${FILE}
+  fi
   N=$((N - 1))
 done
