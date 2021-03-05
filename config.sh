4778959f6f87fdac8e0d17b26ccc86667919973d
Axel Wienberg <axel@wuenschenswert.net>
2021-03-05 14:41:12 +0100

    influxdb

--- config.sh
+++ config.sh
@@ -5,3 +5,6 @@ WLAN_SD_IP=192.168.80.42
 # configure to your liking.
 GRAFANA_ADMIN_PASSWORD=admin
 PG_ADMIN_PASSWORD=topsecret
+INFLUXDB_ADMIN_PASSWORD=topsecrettoo
+INFLUXDB_WRITE_USER_PASSWORD=almostassecret
+INFLUXDB_READ_USER_PASSWORD=notquitesosecret
